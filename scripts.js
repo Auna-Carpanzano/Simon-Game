73789b068010dc987da1268e5019f45209b20e04
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-18 13:17:03 -0700

    Create and call nextStep function

--- scripts.js
+++ scripts.js
@@ -5,7 +5,14 @@ var greenSound = $("#greenSound")[0];
 var steps = [];
 var step = 0;
 
+function nextStep(){
+  var nextColor = [red, blue, yellow, green][Math.floor(Math.random() * 4)];
+  console.log("random color", nextColor);
+}
+
 $(document).ready(function(){
+
+  nextStep();
 
   $("#red").click(function(){
     redSound.play();
