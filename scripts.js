b3d88b8c11a1fdbd75c776fedf5840b9e4b755aa
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-18 13:11:14 -0700

    Add steps and step variables

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ var redSound = $("#redSound")[0];
 var blueSound = $("#blueSound")[0];
 var yellowSound = $("#yellowSound")[0];
 var greenSound = $("#greenSound")[0];
+var steps = [];
+var step = 0;
 
 $(document).ready(function(){
 
