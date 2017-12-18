4609205f257564b3859683d322540f5fcdc594c3
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-18 13:08:49 -0700

    Add click listener to colored buttons to play sound

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
-var redSound= $("#redSound")[0];
-var blueSound= $("#blueSound")[0];
-var yellowSound= $("#yellowSound")[0];
-var greenSound= $("#greenSound")[0];
+var redSound = $("#redSound")[0];
+var blueSound = $("#blueSound")[0];
+var yellowSound = $("#yellowSound")[0];
+var greenSound = $("#greenSound")[0];
+
+$(document).ready(function(){
+
+  $("#red").click(function(){
+    redSound.play();
+  });
+
+  $("#blue").click(function(){
+    blueSound.play();
+  });
+
+  $("#yellow").click(function(){
+    yellowSound.play();
+  });
+
+  $("#green").click(function(){
+    greenSound.play();
+  });
+
+});
