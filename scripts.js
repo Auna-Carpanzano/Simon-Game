600284c81ee87702ff9f890616d7dde9b07f9759
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-18 13:21:51 -0700

    Push the new color to the simonSteps array

--- scripts.js
+++ scripts.js
@@ -6,12 +6,16 @@ var redSound = $("#redSound")[0];
 var blueSound = $("#blueSound")[0];
 var yellowSound = $("#yellowSound")[0];
 var greenSound = $("#greenSound")[0];
-var steps = [];
+var simonSteps = [];
 var step = 0;
 
 function nextStep(){
   var nextColor = [red, blue, yellow, green][Math.floor(Math.random() * 4)];
   console.log("random color", nextColor);
+  simonSteps.push(nextColor);
+  console.log("steps", simonSteps);
+  step++;
+  console.log("step", step);
 }
 
 $(document).ready(function(){
