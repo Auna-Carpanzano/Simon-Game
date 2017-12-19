6150e60dcfbe81dbc811b04ce2c2c4211c456adf
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-18 18:34:09 -0700

    Create sendColor function and pass in each color as parameter when clicked

--- scripts.js
+++ scripts.js
@@ -7,36 +7,67 @@ var blueSound = $("#blueSound")[0];
 var yellowSound = $("#yellowSound")[0];
 var greenSound = $("#greenSound")[0];
 var simonSteps = [];
-var playerSteps = [];
 var step = 0;
 
 function nextStep(){
   var nextColor = [red, blue, yellow, green][Math.floor(Math.random() * 4)];
   console.log("random color", nextColor);
   simonSteps.push(nextColor);
-  console.log("steps", simonSteps);
-  step++;
+  console.log("simonSteps", simonSteps);
   console.log("step", step);
 }
 
-$(document).ready(function(){
-
+function startGame(){
   nextStep();
+}
+
+function sendColor(color){
+  //If the correct color is selected
+  if(color==simonSteps[step]){
+    //If the sequence is completed
+    if(step===simonSteps.length-1){
+      step=0;
+      nextStep();
+      $("#count").empty();
+      $("#count").append(simonSteps.length);
+    }
+    else {
+      //If the sequence is not completed
+      step++;
+    }
+  }
+  //If the correct color is not selected
+  else {
+    alert("You lose!");
+    simonSteps=[];
+    playerSteps=[];
+    step= 0;
+    $("#count").html(0);
+    startGame();
+  }
+}
+
+$(document).ready(function(){
+startGame();
 
   $("#red").click(function(){
     redSound.play();
+    sendColor(red);
   });
 
   $("#blue").click(function(){
     blueSound.play();
+    sendColor(blue);
   });
 
   $("#yellow").click(function(){
     yellowSound.play();
+    sendColor(yellow);
   });
 
   $("#green").click(function(){
     greenSound.play();
+    sendColor(green);
   });
 
 });
